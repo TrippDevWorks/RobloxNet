3c2a92ec9590627bb727482e8c49136be4cafab1
TrippDev <123249000+TrippDevWorks@users.noreply.github.com>
2026-04-26 14:35:16 -0400

    setup the first initalization to split the client between legacy and opencloud.

--- src/RobloxNet/RobloxNet.py
+++ src/RobloxNet/RobloxNet.py
@@ -1,18 +1,29 @@
 import requests
+from .Legacy import Legacy
+from .OpenCloud import OpenCloud
 
 class Auth:
-    def __init__(self, cookie: str):
+    def __init__(self, ClientType: str, Token: str):
+        if not Token:
+            raise ValueError("[AUTH] No authentication Token was provided on initalization, did you forget to add your api key?.")
         self.session = requests.Session()
-        self.session.cookies.set(".ROBLOSECURITY", cookie, domain=".roblox.com")
-        self.xcsrf_token = self._get_xcsrf_token()
-        self.session.headers.update({"X-CSRF-TOKEN": self.xcsrf_token})
-        
+        client_type = ClientType.strip().lower()
+        if client_type == "legacy":
+            self.session.cookies.set(".ROBLOSECURITY", Token, domain=".roblox.com")
+            self.xcsrf_token = self.get_xcsrf()
+            self.session.headers.update({"X-CSRF-TOKEN": self.xcsrf_token})
+            self.client = Legacy(Token, session=self.session)
+        elif client_type == "opencloud":
+            self.session.headers.update({"x-api-key": Token})
+            self.client = OpenCloud(Token, session=self.session)
+        else:
+            raise ValueError(
+                f"Unsupported ClientType: {ClientType}. Choose 'Legacy' or 'OpenCloud'."
+            )
 
     def get_xcsrf(self) -> str:
         response = self.session.post("https://auth.roblox.com/v2/logout")
         token = response.headers.get("x-csrf-token")
-
         if not token:
-            raise RuntimeError("Failed to retrieve X-CSRF-Token.")
-
+            raise RuntimeError("[AUTH] Failed to retrieve X-CSRF-Token.")
         return token
